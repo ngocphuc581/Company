d48e6d2f506010345a9d48e44be988bacc99f9c4
Phuc <ngocphuc00001@gmail.com>
2023-04-18 16:48:27 +0700

    Initial Commit

---
+++ Demo1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Demo1
+//
+//  Created by Zien on 18/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by Zien on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
